4f5c7677799b3dd5d3ed72f402649a828f28c560
pawel <pawel.niewolik@student.hu.nl>
2015-04-16 12:48:18 +0200

    opdracht 1

--- source/ExternalDLL/ExternalDLL/IntensityImageStudent.cpp
+++ source/ExternalDLL/ExternalDLL/IntensityImageStudent.cpp
@@ -1,75 +1,86 @@
 #include "IntensityImageStudent.h"
 
 IntensityImageStudent::IntensityImageStudent() : IntensityImage() {
-	int throwError = 0, e = 1 / throwError; //Throws error without the need to include a header
-	//TODO: Nothing
 }
 
 IntensityImageStudent::IntensityImageStudent(const IntensityImageStudent &other) : IntensityImage(other.getWidth(), other.getHeight()) {
-	int throwError = 0, e = 1 / throwError;
-	//TODO: Create a copy from the other object
+	if (other.getHeight() > 0 && other.getWidth() > 0){
+		int imageSize = other.getWidth() * other.getHeight();
+		imageMap = new Intensity[imageSize];
+		for (int i = 0; i <= imageSize; i++){
+			imageMap[i] = other.getPixel(i);
+		}
+	}
 }
 
 IntensityImageStudent::IntensityImageStudent(const int width, const int height) : IntensityImage(width, height) {
-	int throwError = 0, e = 1 / throwError;
-	//TODO: Initialize pixel storage
+	if (width > 0 && height > 0){
+		int imageSize = width * height;
+		imageMap = new Intensity[imageSize];
+		for (int i = 0; i <= imageSize; i++){
+			imageMap[i] = 0;
+		}
+	}
 }
 
 IntensityImageStudent::~IntensityImageStudent() {
-	int throwError = 0, e = 1 / throwError;
-	//TODO: delete allocated objects
+	delete imageMap;
 }
 
 void IntensityImageStudent::set(const int width, const int height) {
+	int oldWidth = getWidth();
+	int oldHeight = getHeight();
 	IntensityImage::set(width, height);
-	int throwError = 0, e = 1 / throwError;
-	//TODO: resize or create a new pixel storage (Don't forget to delete the old storage)
+	int imageSize = width * height;
+	if (width == oldWidth && height == oldHeight){
+		for (int i = 0; i <= imageSize; i++){
+			imageMap[i] = 0;
+		}
+	}
+	else{
+		delete imageMap;
+		imageMap = new Intensity[imageSize];
+		for (int i = 0; i <= imageSize; i++){
+			imageMap[i] = 0;
+		}
+	}
 }
 
-void IntensityImageStudent::set(const IntensityImageStudent &other) {
+void IntensityImageStudent::set(const IntensityImageStudent &other){
 	IntensityImage::set(other.getWidth(), other.getHeight());
-	int throwError = 0, e = 1 / throwError;
-	//TODO: resize or create a new pixel storage and copy the object (Don't forget to delete the old storage)
+	int imageSize = other.getWidth() * other.getHeight();
+	delete imageMap;
+	imageMap = new Intensity[imageSize];
+	for (int i = 0; i <= imageSize; i++){
+		imageMap[i] = other.getPixel(i);
+	}
 }
+
 
 void IntensityImageStudent::setPixel(int x, int y, Intensity pixel) {
-	int throwError = 0, e = 1 / throwError;
-	//TODO: no comment needed :)
+	int pix = getWidth() * y + x;
+	imageMap[pix] = pixel;
 }
 
 void IntensityImageStudent::setPixel(int i, Intensity pixel) {
-	int throwError = 0, e = 1 / throwError;
-	/*
-	* TODO: set pixel i in "Row-Major Order"
-	*
-	*
-	* Original 2d image (values):
-	* 9 1 2
-	* 4 3 5
-	* 8 7 8
-	*
-	* 1d representation (i, value):
-	* i		value
-	* 0		9
-	* 1		1
-	* 2		2
-	* 3		4
-	* 4		3
-	* 5		5
-	* 6		8
-	* 7		7
-	* 8		8
-	*/
+	imageMap[i] = pixel;
 }
 
 Intensity IntensityImageStudent::getPixel(int x, int y) const {
-	int throwError = 0, e = 1 / throwError;
-	//TODO: no comment needed :)
-	return 0;
+	if (x <= getWidth()){
+		if (y <= getHeight()){
+			int pix = getWidth() * y + x;
+			return imageMap[pix];
+		}
+		else{
+			return 0;
+		}
+	}
+	else{
+		return 0;
+	}
 }
 
 Intensity IntensityImageStudent::getPixel(int i) const {
-	int throwError = 0, e = 1 / throwError;
-	//TODO: see setPixel(int i, RGB pixel)
-	return 0;
+	return imageMap[i];
 }
--- source/ExternalDLL/ExternalDLL/RGBImageStudent.cpp
+++ source/ExternalDLL/ExternalDLL/RGBImageStudent.cpp
@@ -1,76 +1,88 @@
 #include "RGBImageStudent.h"
 
 RGBImageStudent::RGBImageStudent() : RGBImage() {
-	int throwError = 0, e = 1 / throwError; //Throws error without the need to include a header
-	//TODO: Nothing
 }
 
 RGBImageStudent::RGBImageStudent(const RGBImageStudent &other) : RGBImage(other.getWidth(), other.getHeight()) {
-	int throwError = 0, e = 1 / throwError;
-	//TODO: Create a copy from the other object
+	int imageSize =other.getHeight() * other.getWidth();
+	imageStorage = new RGB [imageSize];
+	for (int i = 0; i <= imageSize; i++){
+		imageStorage[i] = other.getPixel(i);
+	}
+
 }
 
-
 RGBImageStudent::RGBImageStudent(const int width, const int height) : RGBImage(width, height) {
-	int throwError = 0, e = 1 / throwError;
-	//TODO: Initialize pixel storage
+
+	if (width > 0 && height > 0){
+		int imageSize = width * height;
+		imageStorage = new RGB[imageSize];
+		for (int i = 0; i <= imageSize; i++){
+			imageStorage[i] = 0;
+		}
+	}
 }
 
 RGBImageStudent::~RGBImageStudent() {
-	int throwError = 0, e = 1 / throwError;
-	//TODO: delete allocated objects
+	delete imageStorage;
 }
 
 void RGBImageStudent::set(const int width, const int height) {
+	int oldWidth = getWidth();
+	int oldHeight = getHeight();
 	RGBImage::set(width, height);
-	int throwError = 0, e = 1 / throwError;
-	//TODO: resize or create a new pixel storage (Don't forget to delete the old storage)
+	if (oldHeight == height && oldWidth == width){
+		int imageSize = height * width;
+		for (int i = 0; i <= imageSize; i++){
+			imageStorage[i] = 0;
+		}
+	}
+	else{
+		delete imageStorage;
+		int imageSize = height * width;
+		imageStorage = new RGB[imageSize];
+		for (int i = 0; i <= imageSize; i++){
+			imageStorage[i] = 0;
+		}
+	}
 }
 
 void RGBImageStudent::set(const RGBImageStudent &other) {
+	int oldWidth = getWidth();
+	int oldHeight = getHeight();
 	RGBImage::set(other.getWidth(), other.getHeight());
-	int throwError = 0, e = 1 / throwError;
-	//TODO: resize or create a new pixel storage and copy the object (Don't forget to delete the old storage)
+	int imageSize = getHeight() * getWidth();
+	if (oldWidth == getHeight() && oldHeight == getHeight()){
+		for (int i = 0; i <= imageSize; i++){
+			imageStorage[i] = other.getPixel(i);
+		}
+	}
+	else{
+		delete imageStorage;
+		imageStorage = new RGB[imageSize];
+		for (int i = 0; i <= imageSize; i++){
+			imageStorage[i] = other.getPixel(i);
+		}
+	}
+
 }
 
 void RGBImageStudent::setPixel(int x, int y, RGB pixel) {
-	int throwError = 0, e = 1 / throwError;
-	//TODO: no comment needed :)
+	
+	int pix = getWidth() * y + x;
+	imageStorage[pix] = pixel;
+
 }
 
 void RGBImageStudent::setPixel(int i, RGB pixel) {
-	int throwError = 0, e = 1 / throwError;
-	/*
-	* TODO: set pixel i in "Row-Major Order"
-	*
-	*
-	* Original 2d image (values):
-	* 9 1 2
-	* 4 3 5
-	* 8 7 8
-	*
-	* 1d representation (i, value):
-	* i		value
-	* 0		9
-	* 1		1
-	* 2		2
-	* 3		4
-	* 4		3
-	* 5		5
-	* 6		8
-	* 7		7
-	* 8		8
-	*/
+	imageStorage[i] = pixel;
 }
 
 RGB RGBImageStudent::getPixel(int x, int y) const {
-	int throwError = 0, e = 1 / throwError;
-	//TODO: no comment needed :)
-	return 0;
+	int pix = getWidth() * y + x;
+	return imageStorage[pix];
 }
 
 RGB RGBImageStudent::getPixel(int i) const {
-	int throwError = 0, e = 1 / throwError;
-	//TODO: see setPixel(int i, RGB pixel)
-	return 0;
+	return imageStorage[i];
 }
